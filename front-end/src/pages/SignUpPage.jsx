a5e2bc94270e01738ceaa3a4b2e90ffe83628a17
Obua Stella <stellaobua123@gmail.com>
2024-01-02 15:04:06 +0100

    Worked on Responsiveness

--- front-end/src/pages/SignUpPage.jsx
+++ front-end/src/pages/SignUpPage.jsx
@@ -8,27 +8,27 @@ import Btn from "../components/btn";
 function SignUpPage() {
   return (
     <div className="max-w-[1440px] mx-auto">
-      <div className="flex justify-center bg-secondaryColor w-full h-lvh">
-        <aside className="w-9/12 hidden lg:block relative ">
+      <div className="max-[380px]:h-full min-[1023px]:h-screen flex justify-center bg-secondaryColor w-full h-lvh">
+        <aside className=" w-9/12 hidden lg:block relative ">
           <img
-            className="absolute w-1/2 top-80 left-10"
+            className="absolute w-1/2 top-0 bottom-0 m-auto"
             src="assets/images/signup-girl.png"
             alt="signup-img"
           />
           <img
-            className=" h-lvh"
+            className="min-[1023px]:h-full h-lvh"
             src="assets/images/Rectangle.png"
             alt="rectangle"
           />
         </aside>
-        <main className="flex flex-col justify-center align-middle w-full lg:mr-12">
+        <main className="max-[1025px]:h-fit flex flex-col justify-center align-middle w-full lg:mr-12 m-auto">
           <img
             className="mt-6 lg:mt-0 w-[358px] mx-auto"
             src="assets/images/logo.png"
             alt="afrikart-logo"
           />
 
-          <form className=" mx-auto w-11/12 sm:w-7/12" action="">
+          <form className="sign-up-form mx-auto w-11/12 sm:w-7/12" action="">
             <h2 className="m-2 text-primaryColor text-xl font-bold">Sign-Up</h2>
             <button
               className="border-none w-full p-3 rounded-lg text-white font-bold border bg-primaryColor"
@@ -74,10 +74,10 @@ function SignUpPage() {
 }
 // TODO:
 // 1. SEMATICS
-// 2. Responsiveness
+// 2. Responsiveness -done
 // 3. Fill up the alt images
 // 4. Work on the button component - done
-// 5. Work on the form
+// 5. Work on the form -done
 // 6. Get icons - done
 // 7. Links
 export default SignUpPage;
