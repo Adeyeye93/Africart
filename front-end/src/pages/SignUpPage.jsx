30c188090a14f877282303415c1c8d8ed094163b
Obua Stella <stellaobua123@gmail.com>
2024-01-03 10:00:58 +0100

    Worked on Sematics for the signup page

--- front-end/src/pages/SignUpPage.jsx
+++ front-end/src/pages/SignUpPage.jsx
@@ -4,12 +4,13 @@ import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { text } from "@fortawesome/fontawesome-svg-core";
 import InputField from "../components/inputField";
 import Btn from "../components/btn";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
   return (
-    <div className="max-w-[1440px] mx-auto">
-      <div className="max-[380px]:h-full min-[1023px]:h-screen flex justify-center bg-secondaryColor w-full h-lvh">
-        <aside className=" w-9/12 hidden lg:block relative ">
+    <section className="max-w-[1440px] min-h-screen mx-auto">
+      <div className="max-[380px]:h-full flex justify-center bg-secondaryColor w-full h-lvh">
+        <aside className=" w-9/12 hidden lg:block relative">
           <img
             className="absolute w-1/2 top-0 bottom-0 m-auto"
             src="assets/images/signup-girl.png"
@@ -21,6 +22,7 @@ function SignUpPage() {
             alt="rectangle"
           />
         </aside>
+
         <main className="max-[1025px]:h-fit flex flex-col justify-center align-middle w-full lg:mr-12 m-auto">
           <img
             className="mt-6 lg:mt-0 w-[358px] mx-auto"
@@ -28,7 +30,10 @@ function SignUpPage() {
             alt="afrikart-logo"
           />
 
-          <form className="sign-up-form mx-auto w-11/12 sm:w-7/12" action="">
+          <form
+            className="sign-up-form mx-auto w-11/12 sm:w-7/12 h-full"
+            action=""
+          >
             <h2 className="m-2 text-primaryColor text-xl font-bold">Sign-Up</h2>
             <button
               className="border-none w-full p-3 rounded-lg text-white font-bold border bg-primaryColor"
@@ -60,24 +65,27 @@ function SignUpPage() {
             <div className="mt-4 text-primaryColor">
               <input className="cursor-pointer" type="checkbox" name="" id="" />
               <p className="mx-1 inline text-sm">
-                Creating an account means you’re okay with our{" "}
-                <span>Terms of Service</span>, <span>Privacy Policy</span>, and
-                our default <span>Notification Settings</span>.
+                Creating an account means you’re okay with our
+                <Link className="font-bold" to="">
+                  {" "}
+                  Terms of Service
+                </Link>
+                ,{" "}
+                <Link className="font-bold" to="">
+                  Privacy Policy
+                </Link>
+                , and our default{" "}
+                <Link className="font-bold" to="">
+                  Notification Settings
+                </Link>
+                .
               </p>
             </div>
             <Btn title="Create Account" />
           </form>
         </main>
       </div>
-    </div>
+    </section>
   );
 }
-// TODO:
-// 1. SEMATICS
-// 2. Responsiveness -done
-// 3. Fill up the alt images
-// 4. Work on the button component - done
-// 5. Work on the form -done
-// 6. Get icons - done
-// 7. Links
 export default SignUpPage;
